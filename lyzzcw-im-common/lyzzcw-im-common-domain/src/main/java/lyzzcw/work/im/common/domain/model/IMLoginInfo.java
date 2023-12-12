e270a0135c7b91683c36b2157793809be000f9e3
lzy <1204375052@qq.com>
2023-12-12 13:49:58 +0800

    common-domain commit

---
+++ lyzzcw-im-common/lyzzcw-im-common-domain/src/main/java/lyzzcw/work/im/common/domain/model/IMLoginInfo.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright 2022-9999 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package lyzzcw.work.im.common.domain.model;
+
+/**
+ * @author lzy
+ * @version 1.0.0
+ * @description 登录信息
+ */
+public class IMLoginInfo {
+    /**
+     * access-token
+     */
+    private String accessToken;
+
+    public IMLoginInfo() {
+    }
+
+    public IMLoginInfo(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+}
